38d5f7fce00c190a8818728d4175efda460cd959
Vemuri-Harshit <73873977+Vemuri-Harshit@users.noreply.github.com>
2021-01-01 16:35:23 +0530

    Update DrawerNavigator.js

--- components/DrawerNavigator.js
+++ components/DrawerNavigator.js
@@ -3,13 +3,18 @@ import {View,Text,TextInput,Modal,KeyboardAvoidingView,StyleSheet,TouchableOpaci
 import{createDrawerNavigator,DrawerItems} from 'react-navigation-drawer'
 import db from '../config';
 import firebase from 'firebase';
+
 import {TabNavigator} from './TabNavigator'
 import SideBarMenu from './SideBarMenu'
 import SettingScreen from '../screens/SettingScreen'
+import MyDonationsScreen from '../screens/MyDonationsScreen'
 
 export const DrawerNavigator = createDrawerNavigator({
     Home:{
         screen:TabNavigator
+    },
+    MyDonations:{
+        screen:MyDonationsScreen
     },
     Settings:{
         screen: SettingScreen
